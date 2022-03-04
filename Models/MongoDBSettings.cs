f7ca290959e7e461cbc50266daa3ce5ca6b913bd
Seth Reeise <southsc22@gmail.com>
2022-03-04 16:43:37 -0500

    Initial commit

---
+++ Models/MongoDBSettings.cs
@@ -0,0 +1,7 @@
+namespace dc_contractform;
+
+public class MongoDBSettings {
+    public string ConnectionURI { get; set;} = null!;
+    public string DatabaseName { get; set;} = null!;
+    public string CollectionName { get; set;} = null!;
+}

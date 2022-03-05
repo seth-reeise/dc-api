4d7c1341c01c32c41181ef88d3ce4577a596a208
Seth Reeise <southsc22@gmail.com>
2022-03-04 22:54:09 -0500

    Add controller and MongoDB service

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
+using DC_CONTRACTFORM.Models;
+using DC_CONTRACTFORM.Services;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// bind setting and Mongo service
+
+builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDB"));
+// Create Singleton instance 
+builder.Services.AddSingleton<MongoDBService>();
 
 // Add services to the container.
 
--- Services/MongoDBService.cs
+++ Services/MongoDBService.cs
@@ -5,9 +5,39 @@ using MongoDB.Bson;
 
 namespace DC_CONTRACTFORM.Services;
 
-public class MongoDBServicce {
+public class MongoDBService {
 
     private readonly IMongoCollection<Contractform> _contractformCollection;
+
+    public MongoDBService(IOptions<MongoDBSettings> mongoDBSettings) {
+        MongoClient client = new MongoClient(mongoDBSettings.Value.ConnectionURI);
+        IMongoDatabase database = client.GetDatabase(mongoDBSettings.Value.DatabaseName);
+        _contractformCollection = database.GetCollection<Contractform>(mongoDBSettings.Value.CollectionName);
+    }
+
+    // can call whatever
+    public async Task CreateAsync(Contractform contractform) {
+        await _contractformCollection.InsertOneAsync(contractform);
+        return;
+    }
+
+    public async Task<List<Contractform>> GetAsync() {
+        return await _contractformCollection.Find(new BsonDocument()).ToListAsync();
+    }
+
+    public async Task UpdateFirstNameAsync(string id, string firstName) {
+        FilterDefinition<Contractform> filter = Builders<Contractform>.Filter.Eq("Id", id);
+        // UpdateDefinition<Contractform> update = Builders<Contractform>.Update.AddToSet<string>("firstName", contractformId);
+        UpdateDefinition<Contractform> update1 = Builders<Contractform>.Update.Set<string>("firstName", firstName);
+        await _contractformCollection.UpdateOneAsync(filter, update1);
+        return;        
+    }
+
+    public async Task DeleteAsync(string id) {
+        FilterDefinition<Contractform> filter = Builders<Contractform>.Filter.Eq("Id", id);
+        await _contractformCollection.DeleteOneAsync(filter);
+        return;
+    } 
 
 }
 
